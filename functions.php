b9cdbf2900255ebb5c69c2f430010c748c544de7
Daiki Itoh <daikii.tech@gmail.com>
2014-11-26 21:39:56 -0800

    Add custom favicon

--- functions.php
+++ functions.php
@@ -5,8 +5,15 @@
  * Author: daikii
  */
 
+/* Google Font stylesheet */
 function load_fonts() {
 	wp_register_style('et-googleFonts', 'http://fonts.googleapis.com/css?family=Codystar|Gilda+Display|Mate+SC|Libre+Baskerville|Cinzel');
 	wp_enqueue_style('et-googleFonts');
 }
 add_action('wp_print_styles', 'load_fonts');
+
+/* custom favicon */
+function blog_favicon() {
+	echo '<link rel="Shortcut Icon" type="image/x-icon" href="'.get_bloginfo('stylesheet_directory').'/images/favicon.ico" />' . "\n";
+}
+add_action('wp_head', 'blog_favicon');
