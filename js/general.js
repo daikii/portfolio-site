330e0403183da9693ae0ac67ad7d91fec603dd98
Daiki Itoh <daikii.tech@gmail.com>
2014-11-27 21:23:53 -0800

    Add front page fade and image opacity fade when hovered

--- js/general.js
+++ js/general.js
@@ -1,15 +1,27 @@
 /*
- * General animations.
+ * General event handlers and animations.
  * Author: daikii
  */
 
 // shortcut for jQuery as $ cannot be used
 var $j = jQuery.noConflict();
 
-// fade in the body contents when page loaded
+// front page effects
 $j(document).ready(function()
 {
-	/*! Fades in whole page on load */
+	// fade in the body contents when page loaded
 	$j('.entry-content').css('display', 'none');
-	$j('.entry-content').fadeIn(500);
+	$j('.entry-content').fadeIn(1500);
+
+	// image fade when cursor hovered
+	$j('img.alignnone.size-medium').hover(
+		function()
+		{
+			$j(this).fadeTo(500, 0.3);
+		},
+		function() 
+		{
+			$j(this).fadeTo(500, 1);
+		}
+	);
 }); 
