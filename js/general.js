4642e572a110266fa60bcfe5bf50577a3b455be8
Daiki Itoh <daikii.tech@gmail.com>
2014-11-27 23:13:25 -0800

    Add Animate.css fade-in-down

--- js/general.js
+++ js/general.js
@@ -13,6 +13,9 @@ $j(document).ready(function()
 	$j('.entry-content').css('display', 'none');
 	$j('.entry-content').fadeIn(1500);
 
+	// Animate.css fade-in-down
+	//$j('img.alignnone.size-medium').addClass('animated fadeInDown');
+
 	// image fade when cursor hovered
 	$j('img.alignnone.size-medium').hover(
 		function()
